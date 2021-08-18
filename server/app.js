24b4e132fc508e7d2f2081cbbffc71a66fb16670
Gal <galpalas89@gmail.com>
2021-08-18 13:22:01 +0300

    create models and finish schema

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://gal:PsjVnuIeWWFML7At@cluster0.vmtp0.mongodb.net/GraphQLMovieList?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("conneted to database");
+});
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
